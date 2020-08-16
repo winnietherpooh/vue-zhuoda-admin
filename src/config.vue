cffdfeb23382bf4b9bb1e59b2d985e507234efce
winnietherpooh <winnietherpooh@163.com>
2020-08-16 22:48:17 +0800

    注意地址

--- src/config.vue
+++ src/config.vue
@@ -1,6 +1,16 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-08-14 15:12:13
+ * @LastEditTime: 2020-08-16 22:45:38
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \admin\src\config.vue
+-->
 <script>
-const IMGCND = 'http://zhuoda-cdn.2blb.com/'
-const APICDN = 'http://127.0.0.1/'
+const IMGCND = 'https://rescdn.zhuodamilk.com/'
+// const APICDN = 'http://www.zdapi.com'
+// const APICDN = 'https://api.zhuodamilk.com/'
+const APICDN = 'https://zhuoda-admin.2blb.com:8767/'
 export default
 {
   IMGCND,
